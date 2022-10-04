bc334c44335d681e8417013f1a95f5cd640a821f
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-10-03 23:01:13 -0400

    tsBuild

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  ignoreBuildErrors: true,
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
-
-module.exports = nextConfig
